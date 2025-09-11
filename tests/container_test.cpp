0309b6eb44dd17f8e291c3d5c44ea619ca0e7437
Dmitry Bogomaz <d.v.bogomaz@gmail.com>
2025-09-11 13:14:46 +0300

    Убрал из шаблона SetUp TearDown. Теперь можно использовать любые конструторы

--- tests/container_test.cpp
+++ tests/container_test.cpp
@@ -7,32 +7,23 @@
 
 // Шаблонный тестовый класс
 template <typename T>
-class ContainerTest : public ::testing::Test {
-protected:
-    void SetUp() override {
-        // Код, выполняемый перед каждым тестом
-        container = new T();
-    }
-    void TearDown() override {
-        // Код, выполняемый после каждого теста
-        delete container;
-    }
-
-    T* container;
-};
+class ContainerTest : public ::testing::Test {};
 
 // Определяем типы для тестирования
 using ContainerTypes = ::testing::Types<MyVector<int>, MyList_1<int>, MyList_2<int>>;
 TYPED_TEST_SUITE(ContainerTest, ContainerTypes);
 
 //- создание контейнера
-TYPED_TEST(ContainerTest, CreateContainer) { EXPECT_EQ(this->container->size(), 0); }
+TYPED_TEST(ContainerTest, CreateContainer) { 
+    TypeParam container;
+    EXPECT_EQ(container.size(), 0); }
 
 //- вставка элементов в конец
 TYPED_TEST(ContainerTest, InsertAtEnd) {
-    this->container->push_back(1);
-    this->container->push_back(2);
-    EXPECT_EQ(this->container->size(), 2);
-    EXPECT_EQ((*this->container)[0], 1);
-    EXPECT_EQ((*this->container)[1], 2);
+    TypeParam container;
+    container.push_back(1);
+    container.push_back(2);
+    EXPECT_EQ(container.size(), 2);
+    EXPECT_EQ(container[0], 1);
+    EXPECT_EQ(container[1], 2);
 }
